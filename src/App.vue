83ad6490edd7ae706ee23c878bab3ad9f15dc76b
Dominik <31939441+EternalDog@users.noreply.github.com>
2019-08-07 20:53:42 +0100

    update 1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <intro />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import intro from './components/intro';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    intro
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', 'Helvetica', 'Arial', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/intro.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="intro_div">
+        <p>--- intro page ---</p>
+
+        <h2>Dominik Bahrynowski</h2>
+        <h3>Software Developer | Web Developer | Volunteer</h3>
+
+
+    </div>
+</template>
+<script>
+export default {
+    name: "intro"
+}
+</script>
+<style lang="scss" scoped>
+#intro_div {
+    border-style: solid;
+}
+
+</style>
+
