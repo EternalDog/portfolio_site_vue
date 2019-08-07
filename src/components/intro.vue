776c0e3f9b82e005a63c22e868c4506829ab5d71
Dominik <31939441+EternalDog@users.noreply.github.com>
2019-08-07 21:45:47 +0100

    intro page scaffolding update

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,21 +1,62 @@
 <template>
-    <div id="intro_div">
-        <p>--- intro page ---</p>
+  <div id="intro_div">
+    <p>--- intro page ---</p>
 
-        <h2>Dominik Bahrynowski</h2>
-        <h3>Software Developer | Web Developer | Volunteer</h3>
+    <img alt="There should be a logo here" id="logo" src="../assets/logo.jpg">
+
+    <h2>Dominik Bahrynowski</h2>
+    <h3>Software Developer | Web Developer | Volunteer</h3>
 
 
+    <div class="flex_container_inner" v-bind:key="projects.name" v-for="projects in projects">
+      <div class="projects_list_item" v-bind:id=projects.link ><p>{{projects.name}}</p></div>
     </div>
+
+
+  </div>
 </template>
 <script>
 export default {
-    name: "intro"
+    name: "intro",
+    data(){
+    return {
+      projects: [
+        {
+          name: "Britannia",
+          link: "aaaa"
+        },
+        {
+          name: "Dice Doge",
+          link: "bbbb"
+        },
+        {
+          name: "something here",
+          link: "cccc"
+        }
+      ]
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 #intro_div {
-    border-style: solid;
+  border-style: solid;
+}
+
+.flex_container_outer {
+  display: flex;
+  flex-direction: row;
+  border-style: solid;
+}
+
+.flex_container_inner {
+  display: flex;
+  flex-direction: column;
+  border-style: solid;
+}
+
+.projects_list_item {
+  color:red;
 }
 
 </style>
